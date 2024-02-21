e4f3f59c2a7e68b9e06d2eaf70be1cbc8f341bcd
t4e1 <jtw5738@naver.com>
2024-02-21 18:10:34 +0900

    2024.02.21 mybatis - dynamic sql
    수업 코드

--- 06_mybatis/chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section03/remix/MenuService.java
+++ 06_mybatis/chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section03/remix/MenuService.java
@@ -9,7 +9,7 @@ import static com.ohgiraffers.section03.remix.Template.getSqlSession;
 /* 설명.
  *  Service 계층 이후부터는 xml방식, javaconfig방식, remix방식 중 어떤 것을 선택할 지 고민해서 진행해야 한다.
  *  remix 방식의 경우는 mybatis 설정은 javaconfig 방식을 취하고, 쿼리를 다루는 것은 xml 방식을 취한다.
- *  DAO에 해당하는 것은 추상메소드를 지닌 인터페이스로 만드는데, 이 인터페이스와 mapper요 xml은 세가지를 준수해야 한다.
+ *  DAO에 해당하는 것은 추상메소드를 지닌 인터페이스로 만드는데, 이 인터페이스와 mapper용 xml은 세가지를 준수해야 한다.
  *  1. xml 파일은 mapper용 인터페이스(DAO에 해당하는 인터페이스)와 같은 경로에 위치해야 한다.
  *  2. xml 파일의 namespace는 mapper용 인터페이스로 작성해야 한다.(풀네임으로)
  *  3. xml 파일명과 mapper용 인터페이스명이 동일해야 한다.
---
+++ 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuMapper.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section01.xml;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MenuMapper {
+
+    List<MenuDTO> selectMenuByPrice(int maxPrice);
+
+    List<MenuDTO> searchMenu(SearchCriteria searchCriteria);
+
+    List<MenuDTO> searchMenuBySupCategory(SearchCriteria searchCriteria);
+
+    List<MenuDTO> searchMenuByRandomMenuCode(List randomlist);
+
+    List<MenuDTO> searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria);
+
+    List<MenuDTO> seacrhMenuByNameOrCategory(Map<String, Object> criteria);
+
+    int updateMenu(Map<String, Object> criteria);
+}
